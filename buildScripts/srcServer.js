b03f15c03678e6a9d51104812d97935ab2b4586d
DIEGO CAMILO MARTINS VIEIRA <dcvieira@tjsp.jus.br>
2021-10-07 21:18:01 -0300

    Add list of users

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -18,6 +18,16 @@ app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname, '../src/index.html'));
 });
 
+app.get('/users', function (req, res) {
+    res.json(
+        [
+            { "id": 1, "firstName": "Diego Camilo", "lastName": "Martins", "email": "diego@email.com" },
+            { "id": 2, "firstName": "Ana Paula", "lastName": "Silva", "email": "ana@email.com" },
+            { "id": 3, "firstName": "Sandra", "lastName": "Duarte", "email": "sandra@email.com" }
+        ]
+    );
+});
+
 app.listen(port, function (err) {
     if (err) {
         console.log(err);
